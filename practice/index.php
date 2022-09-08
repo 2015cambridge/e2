ff5d4ed92458befeaeb3255686a4c077f5684cdb
Mourad Saleh <2015cambridge@gmail.com>
2022-09-08 01:17:59 +0000

    new test

--- practice/index.php
+++ practice/index.php
@@ -1,2 +1,14 @@
-<h1>Practice App</h1>  
-<img src="hes-logo.png" alt="HES logo">
+<!doctype html>
+<html lang='en'>
+
+<head>
+  <title>My DigitalOcean server</title>
+  <meta charset='utf-8'>
+  <link href=data:, rel=icon>
+</head>
+
+<body>
+  <h1>Hello from my DigitalOcean server!</h1>
+</body>
+
+</html>

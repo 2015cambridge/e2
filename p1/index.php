2e9ad7c844620da6862612de230d8a1bf209c67c
“Mourad <2015cambridge@gmail.com>
2022-09-16 23:44:30 +0000

    p1 files

--- p1/index.php
+++ p1/index.php
@@ -1,23 +1,6 @@
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 1</title>
-</head>
-<body>
-    <h2>Mechanics</h2>
-<ul>
-    <li>...</li>
-</ul>
+<?php
 
-<h2>Results</h2>
-<ul>
-    <li>...</li>
-</ul>
-</body>
-</html><h1>Project 1</h1>
+require 'index.view.php';
 
